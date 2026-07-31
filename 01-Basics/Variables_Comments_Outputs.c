30287c85c4a567d0d7dccfd50e5aefb2171ea9d4
Vindhya0712 <svh.aiml2026@gmail.com>
2026-07-31 22:47:43 +0530

    Learning C Basics

---
+++ 01-Basics/Variables_Comments_Outputs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main() {
+    int number = 7;
+    char hashtag = '#';
+    char name = 'Vindhya';
+    float pi = 3.14;
+    int _age = 19;
+    // This is a comment
+    _age = 21;
+    /*You can update previously initialized variables like above.
+    Also, this is a multi-line comment btw*/
+    printf("My name is %s and my age is %d", name, _age);
+    return 0;
+}
