2300b6bc27a02c98ba219b8b190f74dceeed71f7
Vindhya0712 <svh.aiml2026@gmail.com>
2026-08-01 11:00:00 +0530

    Learnt how to store strings in C

--- 01-Basics/Variables_Comments_Outputs.c
+++ 01-Basics/Variables_Comments_Outputs.c
@@ -4,13 +4,15 @@
 int main() {
     int number = 7;
     char hashtag = '#';
-    char name = 'Vindhya';
+    char name[] = "Vindhya";
     float pi = 3.14;
-    int _age = 19;
+    int _age = 21;
     // This is a comment
-    _age = 21;
-    /*You can update previously initialized variables like above.
-    Also, this is a multi-line comment btw*/
-    printf("My name is %s and my age is %d", name, _age);
+    _age = 19;
+    /*
+    You can update previously initialized variables like above.
+    Also, this is a multi-line comment btw
+    */
+    printf("\nMy name is %s and my age is %d.\n", name, _age);
     return 0;
 }
