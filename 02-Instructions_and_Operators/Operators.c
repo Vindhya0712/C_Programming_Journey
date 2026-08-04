770fd5a8dd04bc75b140cdcc574f125ff5890192
Vindhya0712 <svh.aiml2026@gmail.com>
2026-08-04 22:15:43 +0530

    Learnt about Instructions and Operators

---
+++ 02-Instructions_and_Operators/Operators.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main () {
+    int a, b;
+    a = 2, b = 3;
+    int x, y = a * b;
+    x = pow(a, b);
+    printf("%d \n", 3/2);
+    printf("%f \n", 3.0/2);
+    int m = 4 + 9 * 10;
+    printf("%d", m);
+    int n = 4 * 3 / 6 * 2;
+    printf("\n%d \n", n);
+    printf("%d \n", 4 >= 5);
+    printf("%d", 4 == 4);
+    return 0;
+}
---
+++ 02-Instructions_and_Operators/Problem_1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main() {
+    int a = (int) 1.999999;
+    printf("%d", a);
+
+    int x = 5 * 2 - 2 * 3;
+    printf("\n%d", x);
+
+    int y = 5 * 2 / 2 * 3;
+    printf("\n%d", y);
+
+    int z = 5 * (2/2) * 3;
+    printf("\n%d", z);
+
+    int w = 5 + 2 / 2 * 3;
+    printf("\n%d", w);
+    return 0;
+}
