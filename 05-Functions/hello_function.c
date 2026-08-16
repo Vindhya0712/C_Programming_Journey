d379dbbe56828874c72960f3b0b6ecebe360b9a4
Vindhya0712 <svh.aiml2026@gmail.com>
2026-08-16 15:32:00 +0530

    Learning functions in C

---
+++ 05-Functions/hello_function.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// Function declaration + definition in one line
+void printHello();
+void goodbye();
+
+int main() {
+    printHello();
+    goodbye();
+    return 0;
+}
+
+void printHello() {
+    printf("Hey there! I am Vindhya. \n");
+}
+
+void goodbye() {
+    printf("Bye-bye!");
+}
