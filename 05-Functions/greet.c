94ff32b5cd2853d8ce1ef50d338e6064ce7270a1
Vindhya0712 <svh.aiml2026@gmail.com>
2026-08-16 15:53:28 +0530

    Modified my functions

--- 05-Functions/greet.c
+++ 05-Functions/greet.c
@@ -1,30 +1,21 @@
 #include <stdio.h>
 
-int greet(char nationality_code);
+void greet(char nationality_code);
 
 int main() {
     char nationality;
-    int user_choice;
     printf("What is your nationality? Enter 'I' for Indian or 'F' for French: ");
     scanf("%c", &nationality);
-    user_choice = greet(nationality);
-    if (user_choice == 1) {
-        printf("Namaste!");
-    } else if (user_choice == 2) {
-        printf("Bonjour!");
-    } else {
-        printf("Hey there!");
-    }
-
+    greet(nationality);
     return 0;
 }
 
-int greet(char nationality_code) {
+void greet(char nationality_code) {
     if (nationality_code == 'I' || nationality_code == 'i') {
-        return 1;
+        printf("Namaste!");
     } else if (nationality_code == 'F' || nationality_code == 'f') {
-        return 2;
+        printf("Bonjour!");
     } else {
-        return 0;
+        printf("Hey there!");
     }
 }
